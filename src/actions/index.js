74bfe99bb80afa16c7734bab673702cdbaa3f2c9
Marc Davies <marcdaviesriot@gmail.com>
2019-07-21 23:36:35 -0700

    add fetchSongId function

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,3 +15,16 @@ export const changeSong = (newSelectedSongId) => ({
   type: types.CHANGE_SONG,
   newSelectedSongId
 });
+
+export function fetchSongId(title) {
+  return function (dispatch) {
+    const localSongId = v4();
+    title = title.replace(' ', '_');
+    return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=3ee59ea1f59a0f43f3bea2522ec6cd51').then(
+      response => response.json(),
+      error => console.log('An error occurred.', error)
+    ).then(function(json) {
+      console.log('CHECK OUT THIS SWEET API RESPONSE:', json)
+    });
+  };
+}

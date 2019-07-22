ebbab766e71c7daaeff7c9869000b23ae78d6d44
Marc Davies <marcdaviesriot@gmail.com>
2019-07-21 22:38:27 -0700

    import action creator, call it in dispatch

--- src/components/SongDisplay.jsx
+++ src/components/SongDisplay.jsx
@@ -15,11 +15,7 @@ const SongDisplay = ({ dispatch, song }) => {
       <div onClick={e => {
         e.preventDefault();
         if(!(arrayPosition === songArray.length - 1)) {
-          action = {
-            type: 'NEXT_LYRIC',
-            currentSongId: id
-          };
-          dispatch(action);
+          dispatch(nextLyric(id));
         } else {
           action = {
             type: 'RESTART_SONG',

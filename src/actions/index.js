e4482002bf88b6d1186821cb95b1a6266b5226a3
Marc Davies <marcdaviesriot@gmail.com>
2019-07-22 00:47:21 -0700

    add receive song function

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,7 +25,14 @@ export function fetchSongId(title) {
       response => response.json(),
       error => console.log('An error occurred.', error)
     ).then(function(json) {
-      console.log('CHECK OUT THIS SWEET API RESPONSE:', json);
+      if (json.message.body.track_list.length > 0) {
+        const musicMatchId = json.message.body.track_list[0].track.track_id;
+        const artist = json.message.body.track_list[0].track.artist_name;
+        const title = json.message.body.track_list[0].track.track_name;
+        fetchLyrics(title, artist, musicMatchId, localSongId, dispatch);
+      } else {
+        console.log('We couldn\'t locate a song under that ID!');
+      }
     });
   };
 }
@@ -34,4 +41,29 @@ export const requestSong = (title, localSongId) => ({
   type: types.REQUEST_SONG,
   title,
   songId: localSongId
+});
+
+export function fetchLyrics(title, artist, musicMatchId, localSongId, dispatch) {
+  return fetch('http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + musicMatchId + '&apikey=3ee59ea1f59a0f43f3bea2522ec6cd51').then(
+    response => response.json(),
+    error => console.log('An error occurred.', error)
+  ).then(function(json) {
+    if (json.message.body.lyrics) {
+      let lyrics = json.message.body.lyrics.lyrics_body;
+      lyrics = lyrics.replace('"', '');
+      const songArray = lyrics.split(/\n/g).filter(entry => entry!="");
+      dispatch(receiveSong(title, artist, localSongId, songArray));
+    } else {
+      console.log('We couldn\'t locate lyrics for this song!');
+    }
+  });
+}
+
+export const receiveSong = (title, artist, songId, songArray) => ({
+  type: types.RECEIVE_SONG,
+  songId,
+  title,
+  artist,
+  songArray,
+  receivedAt: Date.now()
 });
